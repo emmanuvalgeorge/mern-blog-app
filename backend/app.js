1f196fd98e8ec394c4bc5596a9178e92ec8cba7e
Emmanuval George <emmanuvalg007@gmail.com>
2025-06-02 18:41:10 +0530

    connected to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,31 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/blog'); // Adjust the path as necessary
+const cors = require('cors');
+
 const app = express()
 const port = 3000
+
+app.use(express.json());
+app.use(cors());
+require('dotenv').config()
+console.log(process.env.MONGODB_URL)
+
+main()
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    });
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
